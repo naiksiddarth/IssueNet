d5ac39530dfc612e125543d7434adf70e20356d8
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-11 23:25:29 +0530

    Add code to handel /api/healthcheck route

---
+++ backend/routes/healthcheck.routes.js
@@ -0,0 +1,9 @@
+import { healthCheck } from "../controllers/healthcheck.controllers.js"
+import { Router } from "express"
+
+const router = Router()
+
+router.route("/")
+.get(healthCheck)
+
+export default router
