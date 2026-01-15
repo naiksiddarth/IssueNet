6b4ba55f386f7bbe5b83b28b5db97fe9bd56e75e
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-15 21:31:49 +0530

    Update APIResponse

--- backend/utils/api-response.js
+++ backend/utils/api-response.js
@@ -1,6 +1,9 @@
-class APIResponse{
-    constructor( data ){
+class APIResponse {
+    constructor(statusCode, data, message = "Success") {
+        this.statusCode = statusCode
         this.data = data
+        this.message = message
+        this.success = statusCode < 400
     }
 }
 
