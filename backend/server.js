27a48bfd6751b8afc54e01aebb88d1891bb39df7
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-15 16:32:01 +0530

    Make server to start only after mongodb connecting

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import dotenv from "dotenv"
 import app from './app.js';
-
+import { connectDB } from "./db/connect.db.js"
+import { User } from "./models/user.models.js"
 dotenv.config({
     path: "./.env",
     quiet: true,
 })
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Listening on http://localhost:${process.env.PORT}`)
-})
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Listening on http://localhost:${process.env.PORT}`)
+        })
+    })
