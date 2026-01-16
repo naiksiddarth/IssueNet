e8ea6c13211e09e94c306135702d2f7ed1164e7e
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-16 22:38:06 +0530

    Add code to show errors and include middleware and validator in routes

---
+++ backend/middlewares/validator.middleware.js
@@ -0,0 +1,15 @@
+import { validationResult } from "express-validator"
+import { User } from "../models/user.models.js"
+import { APIError } from "../utils/api-errors.js"
+
+export const validate = async function (req, res, next){
+    const error = validationResult(req)
+    if(error.isEmpty()) {
+        return next()
+    }
+    const extractedErrors = [];
+    error.array().map(err => {extractedErrors.push({[err.path]: err.msg})})
+    
+    throw new APIError(422, "Recieved data is not valid", extractedErrors)
+
+}   
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { registerUser, loginUser } from "../controllers/auth.controllers.js"
+import { validate } from "../middlewares/validator.middleware.js"
+import { registrationVaidator } from "../validators/register.validators.js"
 
 const authRouter = Router()
 
-authRouter.route("/register").post(registerUser)
+authRouter.route("/register").post(registrationVaidator(), validate, registerUser)
 authRouter.route("/login").post(loginUser)
 
 export default authRouter
