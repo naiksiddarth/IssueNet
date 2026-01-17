5c65b16dccfa9f5574668e389e1997b63cef70d7
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-17 15:04:59 +0530

    Add function to generate access and refresh token

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,29 +1,62 @@
 import mongoose from "mongoose"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required:true,
-        unique: true,
-        index: true
+        username: {
+            type: String,
+            required:true,
+            unique: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required:true,
+            unique: true,
+            index: true
+        },
+        password: {
+            type: String,
+            required:true
+        },
+        refreshToken: {
+            type: String
+        }
     },
-    email: {
-        type: String,
-        required:true,
-        unique: true,
-        index: true
-    },
-    password: {
-        type: String,
-        required:true
+    {
+        timestamps: true
     }
-})
+)
 
 userSchema.pre("save", async function () {
     if(!this.isModified("password")) return
     this.password = await bcrypt.hash(this.password, 10)
 })
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        email: this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 const User = mongoose.model("User", userSchema)
 
