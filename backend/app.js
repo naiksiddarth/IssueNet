4baf94b127bdc540ac451b66e67c99275f876eda
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-17 14:55:57 +0530

    Add cookie parser

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ import express from "express"
 import cors from "cors"
 import healthCheckRouter from "./routes/healthcheck.routes.js"
 import authRouter from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(express.json())
-
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.use(cookieParser())
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
