69025b699827a24d455616ed61113856fe21e7a6
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-19 19:27:47 +0530

    Add /api/auth/refresh-token route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser} from "../controllers/auth.controllers.js"
+import { registerUser, loginUser, logoutUser, refreshAccessToken} from "../controllers/auth.controllers.js"
 import { validate } from "../middlewares/validator.middleware.js"
 import { registrationValidator, loginValidator } from "../validators/register.validators.js"
 import { userInjector } from "../middlewares/auth.middleware.js"
@@ -9,5 +9,6 @@ const authRouter = Router()
 authRouter.route("/register").post(registrationValidator(), validate, registerUser)
 authRouter.route("/login").post(loginValidator(), validate, loginUser)
 authRouter.route("/logout").post(userInjector, logoutUser)
+authRouter.route('/refresh-token').post(refreshAccessToken)
 
 export default authRouter
